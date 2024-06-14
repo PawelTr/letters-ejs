f0eddf03c3eda3f987ac4b1a44a23dacfeab6c51
mr-klevoshkin <aleksei.klevtcov@gmail.com>
2024-06-14 22:52:01 +0300

    letters ejs

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const app = express();
+const router = express.Router();
 
 // Устанавливаем EJS как шаблонизатор
 app.set("view engine", "ejs");
 
-// Настройка директории для статических файлов
-app.use(express.static(__dirname + "/public"));
-
-// Обработка запроса на главную страницу
-app.get("/", (req, res) => {
+// Настраиваем рендер страниц
+router.get("/first-letter", (req, res, next) => {
   const data = {
     title: "Заголовок письма",
     message:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum distinctio tenetur vero aliquid possimus vel debitis ipsa doloribus quisquam, esse fugit nulla sapiente quidem neque utvoluptates! Non voluptates atque odio rem.",
   };
-  res.render("index", data);
+  res.render("first-letter", data);
 });
+
+router.get("/second-letter", (req, res, next) => {
+  const data = {
+    code: "230672",
+  };
+  res.render("second-letter", data);
+});
+
+router.get("/third-letter", (req, res, next) => {
+  res.render("third-letter");
+});
+
+// Настройка директории для статических файлов
+app.use(router, express.static(__dirname + "/public"));
+
+// Обработка запроса на главную страницу
 
 // Запуск сервера
 app.listen(3000, () => {
